2d5f1e7e480da5003495b012b0f921b90a8aac8e
cmaP-source <rjj98832@gmail.com>
2025-05-17 17:41:10 -0400

    Add files via upload

--- web/php/Registro.php
+++ web/php/Registro.php
@@ -1,27 +1,22 @@
 <?php
-require('conn.php');
+require('Conn.php');
+session_start();
 
-if(!empty($_POST));
-
-$id = mysqli_real_escape_string($conn, $_POST['id']);
-$usuarios = mysqli_real_escape_string($conn, $_POST['usuario']);
-$contraseña = mysqli_real_escape_string($conn, $_POST['contraseña']);
-
-$shal_pass = sha1($contraseña);
-if($usuarios ==""){
-    echo"Debe escribir un nombre";
-}elseif($contraseña== ""){
-    echo "Debe escribir una contraseña";
-}else
- $mysqli = "INSERT INTO usuarios (id,usario,contraseña) VALUES ('$id',)
-
-
-
-
-
-
-
-
+if(isset($_SESSION['id_usuario'])){ 
+    header("location: index.html");
 }
 
+if(!empty($_POST)) 
+{
+    $id = mysqli_real_escape_string($mysqli, $_POST['id']);
+    $usuario = mysqli_real_escape_string($mysqli, $_POST['usuario']);
+    $contraseña = mysqli_real_escape_string($mysqli, $_POST['contraseña']);
+
+
+    $sha1_pass = sha1($contraseña);
+
+    $sql = "INSERT INTO USUARIO (id, usuario, contraseña) VALUES ('".$id."','".$usuario."','".$contraseña."')";
+    $result = $mysqli -> query($sql);
+    echo "registrado";
+}
 ?>
